a5c868dc6f4175efc427ad02b852b6fa04feed65
homelyseven250 <georgee.keylock@outlook.com>
2021-10-08 18:08:54 +0100

    Add comments to app.py

--- app.py
+++ app.py
@@ -22,11 +22,12 @@ botSID = None
 
 @login_manager.user_loader
 def load_user(user_id):
+    # This is very simple - I copied and pasted it from some other code of mine.
     user = User()
     user.id = user_id
     return user
 
-
+# Register OAuth for discord.
 discord = oauth.register(
     name='discord',
     client_id=app.config['DISCORD_BOT_ID'],
@@ -40,15 +41,15 @@ discord = oauth.register(
     prompt='none'
 )
 
-
+# Use the default flask_login user mixin.
 class User(flask_login.UserMixin):
     pass
 
-
+# Get an invite url for the guild id. This will probably be altered for production.
 def getInviteURL(guild_id):
-    return f'https://discord.com/oauth2/authorize?client_id=887734783139012629&scope=bot%20applications.commands&permissions=7784103761&guild_id={guild_id}&disable_guild_select=true&response_type=code&redirect_uri={url_for("invite_callback", _external=True)}'
+    return f'https://discord.com/oauth2/authorize?client_id={app.config["DISCORD_BOT_ID"]}&scope=bot%20applications.commands&permissions=7784103761&guild_id={guild_id}&disable_guild_select=true&response_type=code&redirect_uri={url_for("invite_callback", _external=True)}'
 
-
+# Check if a bot is in a specific guild via the discord API.
 def checkGuild(id):
     inGuilds = getBotGuilds()
     for guild in inGuilds:
@@ -56,7 +57,7 @@ def checkGuild(id):
             return True
     return False
 
-
+# Get a list of all guilds the bot is in.
 def getBotGuilds():
     client = requests.session()
     client.headers.update(
@@ -65,7 +66,7 @@ def getBotGuilds():
         app.config['DISCORD_API_BASE_URL'] + 'users/@me/guilds').text)
     return guilds
 
-
+# Use the discord API to get a list of all channels in a guild.
 def getChannels(guild_id):
     client = requests.session()
     client.headers.update(
@@ -74,7 +75,7 @@ def getChannels(guild_id):
         app.config['DISCORD_API_BASE_URL'] + f'guilds/{guild_id}/channels').text)
     return channels
 
-
+# Get data for a specific channel id.
 def getChannel(channel_id):
     client = requests.session()
     client.headers.update(
@@ -83,7 +84,7 @@ def getChannel(channel_id):
         app.config['DISCORD_API_BASE_URL'] + f'channels/{channel_id}').text)
     return channel
 
-
+# Redirect to discord login
 @app.route('/login/discord')
 def login():
     return discord.authorize_redirect()
@@ -91,19 +92,22 @@ def login():
 
 @app.route('/auth/discord')
 def auth():
-    token = discord.authorize_access_token()
-    authdata = json.loads(discord.get('users/@me').text)
+    token = discord.authorize_access_token() # Get the token
+    authdata = json.loads(discord.get('users/@me').text) # Get user data
+    # Get data
     id = authdata['id']
+    # Combine username and the number after it,
+    #  as this is what the end user will expect.
     username = authdata['username']+'#'+authdata['discriminator']
     email = authdata['email']
-    # Create a user object
+    # Create a user object with email, id and the username
     user = User()
     user.id = id
     user.username = username
     user.email = email
-    storagePath = os.path.join('data', str(id))
-    print(storagePath)
-    if os.path.exists(storagePath):
+    storagePath = os.path.join('data', str(id)) # Where we will store user's data
+    if os.path.exists(storagePath): # Is the user data already existing?
+        # If so, merge data
         userStorage = json.loads(
             open(os.path.join('data', id, 'user.json')).read())
         userStorage['username'] = username
@@ -114,6 +118,7 @@ def auth():
         with open(os.path.join('data', id, 'user.json'), 'w') as outfile:
             json.dump(userStorage, outfile)
     else:
+        # Else make a new file and folders and dump the data
         os.makedirs(os.path.join('data', id))
         userStorage = {}
         userStorage['username'] = username
@@ -125,13 +130,13 @@ def auth():
         with open(os.path.join('data', id, 'user.json'), 'w') as outfile:
             json.dump(userStorage, outfile)
 
-    flask_login.login_user(user)
-    return redirect(url_for('dashboard'))
+    flask_login.login_user(user) # Actually log in the user
+    return redirect(url_for('dashboard')) # Redirect to dashboard
 
 
 @app.route('/')
 def index():
-    if flask_login.current_user.is_authenticated:
+    if flask_login.current_user.is_authenticated: # If you're logged in, you get the dashboard
         return redirect(url_for('dashboard'))
     return render_template('index.html')
 
@@ -140,37 +145,32 @@ def index():
 @flask_login.login_required
 def dashboard():
     user = json.loads(open(os.path.join(
-        'data', flask_login.current_user.get_id(), 'user.json')).read())
-    if os.path.exists(os.path.join('data', flask_login.current_user.get_id(), 'guilds.json')):
-        ownedGuilds = json.loads(open(os.path.join(
-            'data', flask_login.current_user.get_id(), 'guilds.json')).read())
-    else:
-        client = requests.session()
-        client.headers.update(
-            {'Authorization': 'Bearer ' + user["discord_token"]["access_token"]})
-        guilds = json.loads(client.get(
-            app.config['DISCORD_API_BASE_URL'] + 'users/@me/guilds').text)
-        ownedGuilds = []
-        for guild in guilds:
-            if guild['owner'] == True:
-                ownedGuilds.append(guild)
-        json.dump(ownedGuilds, open('data/'+user["id"]+'/guilds.json', 'w'))
-    return render_template('dashboard.html', username=user["username"], guilds=ownedGuilds, user=user, botGuilds=getBotGuilds())
+        'data', flask_login.current_user.get_id(), 'user.json')).read()) # Load user data
+    client = requests.session()
+    client.headers.update(
+        {'Authorization': 'Bearer ' + user["discord_token"]["access_token"]}) # Use the saved OAuth2 token
+    guilds = json.loads(client.get(
+        app.config['DISCORD_API_BASE_URL'] + 'users/@me/guilds').text) # Get all guilds the user is a member of
+    ownedGuilds = []
+    for guild in guilds:
+        if guild['owner'] == True:
+            ownedGuilds.append(guild) # Filter it down to only owned guilds
+    return render_template('dashboard.html', username=user["username"], guilds=ownedGuilds, user=user)
 
 
 @app.route('/logout')
 def logout():
-    flask_login.logout_user()
+    flask_login.logout_user() # Logout the user
     return redirect(url_for('index'))
 
 
 @app.route('/dashboard/guild/<guild_id>')
 @flask_login.login_required
 def guild(guild_id):
-    if guild_id.isdecimal():
-        if checkGuild(guild_id):
+    if guild_id.isdecimal(): # Is the ID a number?
+        if checkGuild(guild_id): # Is the bot in the guild?
             user = json.loads(open(os.path.join(
-                'data', flask_login.current_user.get_id(), 'user.json')).read())
+                'data', flask_login.current_user.get_id(), 'user.json')).read()) # Load user data
             if not os.path.exists(os.path.join('data', guild_id, 'guild.json')):
                 os.makedirs(os.path.join('data', werkzeug.utils.secure_filename(guild_id)))
                 guild = open(os.path.join('data', guild_id, 'guild.json'), 'w')
@@ -187,14 +187,16 @@ def guild(guild_id):
         else:
             return redirect(getInviteURL(guild_id))
     else:
-        abort(403)
+        abort(400) # If you're curious Acid, see here: https://mzl.la/3FEFpdR
+
+# Test code
+# @app.route('/dashboard/guild/<guild_id>/embed')
+# @flask_login.login_required
+# def embedRoute(guild_id):
+#     return "OK"
 
 
-@app.route('/dashboard/guild/<guild_id>/embed')
-@flask_login.login_required
-def embedRoute(guild_id):
-    return "OK"
-
+# OLD CODE BELOW, PRESERVED IN CASE NEEDED IN FUTURE
 # @app.route('/dashboard/guild/<guild_id>/channels/<channel_id>/msg')
 # @flask_login.login_required
 # def guild_msg(guild_id, channel_id):
@@ -240,26 +242,28 @@ def embedRoute(guild_id):
 @flask_login.login_required
 def admin():
     user = json.loads(open(os.path.join(
-        'data', flask_login.current_user.get_id(), 'user.json')).read())
-    if user['admin'] == True:
-        return render_template('admin.html', user=user, username=user['username'], allGuildsLength=len(getBotGuilds()))
+        'data', flask_login.current_user.get_id(), 'user.json')).read()) # Load user data
+    if user['admin'] == True: # Is the user an admin?
+        return render_template('admin.html', user=user, username=user['username'], allGuildsLength=len(getBotGuilds())) # If so, render and return the admin page
     else:
-        return redirect(url_for('dashboard'))
+        return redirect(url_for('dashboard')) # Redirect to the normal dashboard if not
 
+# Requested after a succeddful invite
 @app.route('/invite/callback')
 def invite_callback():
-    if request.args.get("guild_id").isdecimal():
+    if request.args.get("guild_id").isdecimal(): # Check if the guild_id is a number
+         # Redirect to the dashboard page for that guild, for a smooth user experience
         return redirect(f'/dashboard/guild/{request.args.get("guild_id")}')
     else:
-        abort(403)
+        abort(400)
 
+# Ping!
 @socket.on('ping')
 def pingSocket():
     socket.emit('pong')
-    socket.emit('reportEvent', {'innerHTML': render_template('report.html', user='AcidFilms', message='Test'), 'user': 'AcidFilms'})
 
 
-
+# Send the favicon (the logo)
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'logo.png', mimetype='image/png')
@@ -267,43 +271,45 @@ def favicon():
 @socket.on('connect')
 def connection(data):
     global botSID
-    if data:
-        if data['key'] == app.config['BOT_KEY']:
+    if data: # Has authdata been presented
+        if data['key'] == app.config['BOT_KEY']: # If so, is it correct?
             botSID = request.sid
         else:
             print("Bot presented incorrect auth key - rejecting")
             print(data['key'])
+            return False # Kick that nasty un-authed thingy off - yuck! LOL
 
 @socket.on('pingBot')
 def pingBot():
-    if botSID:
-        socket.emit('ping', to=botSID)
+    if botSID: # Do we have a connection ID for the bot?
+        socket.emit('ping', to=botSID) # Send a ping to the bot
+        # Tell the requesting web browser that a ping has been sent - currently unused except for manual debugging
         socket.emit('pinged', to=request.sid)
 
-@socket.on('pong')
+@socket.on('pong') # When we get a pong
 def pong():
-    socket.emit('botStatus', {"status": "OK"}, broadcast=True)
+    socket.emit('botStatus', {"status": "OK"}, broadcast=True) # Send all socketio clients the bot's status
 
-@socket.on('settingsChange')
+@socket.on('settingsChange') # Setting changes from the web browser
 def settingsChange(data):
-    socket.emit('settingsChange', data, to=botSID)
+    socket.emit('settingsChange', data, to=botSID) # Send them on directly to the bot
 
 @socket.on('getAllCommands')
 def getAllCommands():
-    socket.emit('getAllCommands', {"sid": request.sid}, to=botSID)
+    socket.emit('getAllCommands', {"sid": request.sid}, to=botSID) # Send a request for all commands to the bot, sending the requester's SID for the callback
 
 
 @socket.on('getDisabledCommands')
 def getAllCommands():
-    socket.emit('getDisabledCommands', {"sid": request.sid}, to=botSID)
+    socket.emit('getDisabledCommands', {"sid": request.sid}, to=botSID) # Send a request for all disabled commands to the bot, sending the requester's SID for the callback
 
 @socket.on('disabledCommands')
 def disabledCommands(data):
-    socket.emit('disabledCommands', data, to=data['sid'])
+    socket.emit('disabledCommands', data, to=data['sid']) # When we get the data back, send it to the requester's SID
 
 @socket.on('enableCommand')
 def enableCommand(data):
-    socket.emit('enableCommand', data, to=botSID)
+    socket.emit('enableCommand', data, to=botSID) # Requests a command to be enabled - UNIMPLEMENTED ON BOT
 
 @socket.on('allCommands')
 def allCommands(data):
@@ -320,4 +326,4 @@ def allCommands(data):
 #     socket.emit('getWarnings', {"guildID": data['guildID']}, to=botSID)
 
 if __name__ == '__main__':
-    socket.run(app)
+    socket.run(app) # Run it if not using flask debugger
